ebb5c2e04b6d66f0e90b037b962b6219572b6078
Robert O'Farrell <robert.ofarrell@gmail.com>
2019-08-29 23:24:03 +0000

    Fixes reference to iwad directory

--- bin/summon.bash
+++ bin/summon.bash
@@ -3,7 +3,7 @@
 /usr/bin/zandronum-server \
   -host \
   -port 10666 \
-  -iwad "/home/zandronum/iwads/doom1.wad" \
+  -iwad "/home/zandronum/iwad/doom1.wad" \
   +exec "/home/zandronum/config/default.cfg" \
   +CrashLog_Dir "/home/zandronum/logs/docker-doom.log" \
   +CrashLogs 2 \
